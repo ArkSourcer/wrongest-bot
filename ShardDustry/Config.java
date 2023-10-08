f185f93056df8dc819019fac6acca93645dc2d0e
Vector <106137239+Vect0r0@users.noreply.github.com>
2023-10-08 11:38:39 -0700

    Hola
    ugu

---
+++ ShardDustry/Config.java
@@ -0,0 +1,15 @@
+package ShardDustry;
+
+import java.util.*;
+
+public class Config {
+    public boolean activeDiscordBot;
+    public String discordToken;
+    public String serverID;
+    public boolean activeEmojiReact;
+    public List<EmojiReactor> emojiReact = Arrays.asList(
+            new EmojiReactor(1,"egg","U+1F95A"),
+            new EmojiReactor(1,"ega","U+1F383"),
+            new EmojiReactor(2,"que","so")
+    );
+}
