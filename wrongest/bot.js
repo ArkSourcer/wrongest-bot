663412ce6e0e5a20f90d55a56909693cad09f100
Arksource <93954648+ArkSourcer@users.noreply.github.com>
2023-10-08 00:59:51 -0400

    Update bot.js

--- wrongest/bot.js
+++ wrongest/bot.js
@@ -1,3 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js")
+const client = new Discord.Client()
 
-// paquete de discord.js okno//
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+//respuesta//
+
+client.on("message", msg => {
+  if (msg.content === "ne") {
+    msg.reply("gro");
+  }
+})
+
+client.login(process.env.TOKEN)
