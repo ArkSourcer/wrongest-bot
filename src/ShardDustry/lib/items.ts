3eabb35a716a84896d5900638f0addc37f96d287
Arksource <93954648+ArkSourcer@users.noreply.github.com>
2024-01-30 18:37:27 -0400

    Update items.ts

--- src/ShardDustry/lib/items.ts
+++ src/ShardDustry/lib/items.ts
@@ -1,25 +1,27 @@
-import type { ItemName } from 'wrongest-bot';
+//import type { ItemName } from 'wrongest-bot';
 //caracter..
-export const items: ItemName[] = [
-  'copper',
-  'lead',
-  'metaglass',
-  'graphite',
-  'sand',
-  'coal',
-  'titanium',
-  'thorium',
-  'scrap',
-  'silicon',
-  'plastanium',
-  'phase-fabric',
-  'surge-alloy',
-  'blast-compound',
-  'pyratite',
-  'beryllium',
-  'tungsten',
-  'oxide',
-  'carbide',
-  'fissile-matter',
-  'dormant-cyst',
-];
+//export const items: ItemName[] = [
+ // 'copper',
+ // 'lead',
+ // 'metaglass',
+ // 'graphite',
+ // 'sand',
+///'coal',
+ // 'titanium',
+//  'thorium',
+//  'scrap',
+ // 'silicon',
+//  'plastanium',
+ // 'phase-fabric',
+ // 'surge-alloy',
+ // 'blast-compound',
+ // 'pyratite',
+//  'beryllium',
+  //'tungsten',
+ // 'oxide',
+ // 'carbide',
+  //'fissile-matter',
+  //'dormant-cyst',
+//];
+
+//uso futuro
