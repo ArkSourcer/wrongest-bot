bd175563056bb2e550c86300c1c4e7648c61f48a
Arksource <93954648+ArkSourcer@users.noreply.github.com>
2024-01-30 18:20:38 -0400

    Create items.ts

---
+++ src/ShardDustry/lib/items.ts
@@ -0,0 +1,25 @@
+import type { ItemName } from 'mindustry-schematic-parser';
+//caracter..
+export const items: ItemName[] = [
+  'copper',
+  'lead',
+  'metaglass',
+  'graphite',
+  'sand',
+  'coal',
+  'titanium',
+  'thorium',
+  'scrap',
+  'silicon',
+  'plastanium',
+  'phase-fabric',
+  'surge-alloy',
+  'blast-compound',
+  'pyratite',
+  'beryllium',
+  'tungsten',
+  'oxide',
+  'carbide',
+  'fissile-matter',
+  'dormant-cyst',
+];
